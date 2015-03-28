129ac478286f506543644b288ddffb9dddb2dc0e
Robin van Nunen <robin@vnunen.nl>
2015-03-29 00:05:00 +0100

    Update popup window / icon

--- src/js/background.js
+++ src/js/background.js
@@ -1,16 +1,60 @@
 
+// Variables
+var authenticated = false;
+var streamersCount = 0;
+
+
+// Update popup window and icon
+function updateStatus() {
+    var popup = "";
+    var img = 'icon-19-off';
+
+    if(authenticated) {
+        if(streamersCount > 5){
+            img = 'icon-19-on';
+        } else {
+            img = 'icon-19-' + streamersCount;
+        }
+
+        if(streamersCount > 0){
+            popup = "popup.html";
+        }
+    }
+
+    chrome.browserAction.setPopup({ popup: popup });
+    chrome.browserAction.setIcon({ 'path': 'img/' + img + '.png' });
+}
+
+// Update streams
+function updateStreams(data) {
+
+}
+
 // Update tick
 function update() {
     setTimeout(update, REFRESH_AMOUNT * 1000);
 
     Twitch.getStatus(function(err, status) {
+        authenticated = status.authenticated;
 
-        if (!status.authenticated) {
+        if (authenticated) {
+            Twitch.api({url: 'streams/followed'}, function(err, data) {
+                if(err){
+                    console.error(err);
+                    return;
+                }
+
+                updateStreams(data);
+                updateStatus();
+            });
+        } else {
             Twitch.login({
                 popup: true,
                 scope: ['user_read'],
                 redirect_uri: chrome.extension.getURL('verify.html')
             });
+
+            updateStatus();
         }
 
     })
@@ -18,6 +62,7 @@ function update() {
 
 // Login event
 Twitch.events.addListener('auth.login', function() {
+    authenticated = true;
     console.log("Authenticated: " + Twitch.getToken());
 });
 
