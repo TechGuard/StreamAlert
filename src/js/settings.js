1afd933c5c4cb15fca75936965a15ab2fa83326f
Robin van Nunen <robin@vnunen.nl>
2016-04-22 01:08:41 +0200

    [ADDED] More screenshots.

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,3 +1,3 @@
-var CLIENT_ID = '1fl3yx4f04icp6j5aw6kmab8pbf8fyr';
+var CLIENT_ID = '6rz2yjgyv0fchnwifra8f2j8quhfssx'; // 1fl3yx4f04icp6j5aw6kmab8pbf8fyr
 var REFRESH_AMOUNT = 15; // seconds
 var ALERT_IGNORE_TIME = 10; // minutes
